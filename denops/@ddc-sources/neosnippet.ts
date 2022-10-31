7656db71bf33312639cebd83933c4f777bae91b8
Ryuhei Yoshida <yoshida.ryuhei0112@gmail.com>
2022-10-31 17:32:56 +0900

    Update neosnippet.ts

--- denops/@ddc-sources/neosnippet.ts
+++ denops/@ddc-sources/neosnippet.ts
@@ -2,11 +2,11 @@ import {
   BaseSource,
   Candidate,
   Context,
-} from "https://deno.land/x/ddc_vim@v0.13.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddc_vim@v0.13.0/deps.ts#^";
+} from "https://deno.land/x/ddc_vim@v2.0.0/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddc_vim@v2.0.0/deps.ts";
 
 export class Source extends BaseSource<{}> {
-  async gatherCandidates(args: {
+  async gather(args: {
     denops: Denops;
     context: Context;
   }): Promise<Candidate[]> {
